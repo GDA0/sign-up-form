7db71128c564a263fb6d1046de7d675fd66016bb
GDA0 <gideondelaliadeti0@gmail.com>
2024-06-28 07:37:30 +0000

    Work on the first col of the main

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Form validation
-const form = document.querySelector(".needs-validation");
-form.addEventListener(
-	"submit",
-	(event) => {
-		if (!form.checkValidity()) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
+// const form = document.querySelector(".needs-validation");
+// form.addEventListener(
+// 	"submit",
+// 	(event) => {
+// 		if (!form.checkValidity()) {
+// 			event.preventDefault();
+// 			event.stopPropagation();
+// 		}
 
-		form.classList.add("was-validated");
-	},
-	false
-);
+// 		form.classList.add("was-validated");
+// 	},
+// 	false
+// );
