a75a8bdf2e70b4f6c4b8b7ec116ab3659476e412
GDA0 <gideondelaliadeti0@gmail.com>
2024-06-28 12:19:54 +0000

    Format JS with standard --fix

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 // Form validation
-const form = document.querySelector(".needs-validation");
+const form = document.querySelector('.needs-validation')
 
 form.addEventListener(
-  "submit",
+  'submit',
   (event) => {
-    const passwordInput = form.querySelector("#password");
-    const confirmPasswordInput = form.querySelector("#confirm-password");
-    const password = passwordInput.value;
-    const confirmPassword = confirmPasswordInput.value;
+    const passwordInput = form.querySelector('#password')
+    const confirmPasswordInput = form.querySelector('#confirm-password')
+    const password = passwordInput.value
+    const confirmPassword = confirmPasswordInput.value
 
     // Reset custom validation messages and feedback display
-    confirmPasswordInput.setCustomValidity("");
+    confirmPasswordInput.setCustomValidity('')
     const invalidFeedbacks =
-      confirmPasswordInput.parentNode.querySelectorAll(".invalid-feedback");
-    invalidFeedbacks.forEach((feedback) => (feedback.style.display = "none"));
+      confirmPasswordInput.parentNode.querySelectorAll('.invalid-feedback')
+    invalidFeedbacks.forEach((feedback) => (feedback.style.display = 'none'))
 
     // Check if confirm password is empty
     if (!confirmPassword) {
-      confirmPasswordInput.setCustomValidity("Please confirm your password.");
+      confirmPasswordInput.setCustomValidity('Please confirm your password.')
       confirmPasswordInput.parentNode.querySelector(
-        ".invalid-feedback"
-      ).style.display = "block";
+        '.invalid-feedback'
+      ).style.display = 'block'
     }
     // Check if passwords match
     else if (password !== confirmPassword) {
-      confirmPasswordInput.setCustomValidity("Passwords do not match.");
+      confirmPasswordInput.setCustomValidity('Passwords do not match.')
       confirmPasswordInput.parentNode.querySelector(
-        ".invalid-feedback.mismatch"
-      ).style.display = "block";
+        '.invalid-feedback.mismatch'
+      ).style.display = 'block'
     }
 
     // If form is invalid, prevent submission
     if (!form.checkValidity()) {
-      event.preventDefault();
-      event.stopPropagation();
+      event.preventDefault()
+      event.stopPropagation()
     }
 
-    form.classList.add("was-validated");
+    form.classList.add('was-validated')
   },
   false
-);
+)
